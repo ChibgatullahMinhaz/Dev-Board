41a7976d4af705f6f5dae1172c9d6fc987dcd1d4
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-03-01 04:47:40 +0600

    fixed undefine issue

--- javaScript/script.js
+++ javaScript/script.js
@@ -9,37 +9,44 @@ document.getElementById('dayShort').innerText = GetDate();
 document.getElementById('FullDate').innerText = getFullDate();
 const time = getTime();
 
-// count
-let tasktCount = parseFloat(document.getElementById('taskAssign').innerText);
-let navCount= parseFloat(document.getElementById('navCount').innerText);
-const conMessageCount = 0;
+
+// // Initial counts
+let tasktCount = tasked.length;
+let navCount = parseFloat(document.getElementById('navCount').innerText);
+let conMessageCount = 0;  
+
+document.getElementById('taskAssign').innerText = tasktCount;
+document.getElementById('navCount').innerText = navCount;
 
 const allbtns = document.querySelectorAll(".complete-btn");
 for (let i = 0; i < allbtns.length; i++) {
-    const btn = allbtns[i];
-   btn.addEventListener('click', function (event) {
+    allbtns[i].dataset.taskTitle = tasked[i].taskTitle;
+    allbtns[i].addEventListener('click', function (event) {
         event.preventDefault();
-        tasktCount--
-        navCount++
+
+        tasktCount--;
+        navCount++;
         document.getElementById('taskAssign').innerText = tasktCount;
         document.getElementById('navCount').innerText = navCount;
-        alert('completed')
-        const cardTitle = btn.taskTitle;
-        const historyContainer = document.getElementById('history');
-        const hDiv =document.createElement('div');
-        hDiv.innerHTML = `
-         <div class="bg-[#f4f7ff] p-4 rounded-2xl">
-        <p class="text-slate-600">You have Complete The Task ${cardTitle}  ${time} </p>
-    </div>
-        `
-       historyContainer.appendChild(hDiv);
-       btn.setAttribute("disabled", "true")
+        alert('Board Updated Successfully.');
+        const cardTitle = allbtns[i].dataset.taskTitle;
 
-       if (conMessageCount === tasktCount) {
-        alert('congrass')
-       }
+        // Update history
+        const historyContainer = document.getElementById('history');
+        const hDiv = document.createElement('div');
+        hDiv.innerHTML = `
+            <div class="bg-[#f4f7ff] p-4 rounded-2xl">
+                <p class="text-slate-600">You have completed the task ${cardTitle} ${time}</p>
+            </div>
+        `;
+        historyContainer.appendChild(hDiv);
+
+        // Disable the button
+        allbtns[i].setAttribute("disabled", "true");
+
+        // Check if all tasks are completed
+        if (conMessageCount === tasktCount) {
+            alert('Congrats!!! You have completed all the current tasks.');
+        }
     });
 }
-
-
-
